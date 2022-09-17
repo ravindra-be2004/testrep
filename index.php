eb00f6c64551909704fbaef66cb34d0ac8e13d00
Daleep Singh Bais <57488638+dbais76@users.noreply.github.com>
2022-09-17 10:41:28 +0530

    Index.php file created

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 style="color: blue; font-size:48px">This is Blue Deployment!</h1>
+<?php
+
+print "<br>";
+print "<br>";
+print "<font color=blue size='4pt'>Demos running on <b>Red Hat OpenShift</b> PHP Builder Image with PHP version " . PHP_VERSION .  "\n"; 
+?>
+
+</body>
+</html>
