9f9be9fa1477585539a74e7e82e555a49af7cf27
Daleep Singh Bais <57488638+dbais76@users.noreply.github.com>
2022-09-17 10:53:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1 style="color: red; font-size:48px">Welcome to Red Hat OpenShift Container Platform !! </h1>
+<h1 style="color: red; text-align: center; font-size:48px">          Welcome to Red Hat OpenShift Container Platform !! </h1>
 <?php
 
 print "<br>";
 print "<br>";
-print "<font color=blue size='4pt'>Demos running on <b>Red Hat OpenShift</b> PHP Builder Image with PHP version " . PHP_VERSION .  "\n"; 
+print "<font color=blue size='4pt'>  Demos running on <b>Red Hat OpenShift</b> PHP Builder Image with PHP version " . PHP_VERSION .  "\n"; 
 ?>
 
 </body>
