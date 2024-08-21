d1e4cf7516896509082846a3708a82a4ad6596f5
Daleep Singh Bais <daleepbais@gmail.com>
2024-08-21 11:10:07 +0530

    added image

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 <body>
     <h1 style="color: red; text-align: center; font-size:48px"> Welcome to Red Hat OpenShift Container Platform !! </h1>
     <?php
-<center>
-<p>"<font color=blue size='5pt'>                Demos running on <b>Red Hat OpenShift</b> PHP Builder Image with PHP version " . PHP_VERSION .  </p>"\n"; 
-</center>
+print "<font color=blue size='5pt'>                Demos running on <b>Red Hat OpenShift</b> PHP Builder Image with PHP version " . PHP_VERSION .  "\n"; 
 ?>
 <br />
 <br />
